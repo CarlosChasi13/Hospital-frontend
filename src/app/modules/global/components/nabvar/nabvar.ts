229c52ea7a0fcd10587348f61ff8d7cbc886fec0
CarlosChasi13 <cachasi3@espe.edu.ec>
2026-08-13 21:10:47 -0500

    creacion de modulos globales

---
+++ src/app/modules/global/components/nabvar/nabvar.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nabvar',
+  imports: [],
+  templateUrl: './nabvar.html',
+  styleUrl: './nabvar.scss',
+})
+export class Nabvar {}
