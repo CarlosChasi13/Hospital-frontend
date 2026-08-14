19141aacc7ad005f020c66888918f25045a3d2cb
CarlosChasi13 <cachasi3@espe.edu.ec>
2026-08-14 06:51:35 -0500

    archivo de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Nabvar } from './modules/global/components/nabvar/nabvar';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Nabvar,
+        loadChildren: ()=> import('./rutas.module').then(m => m.RutasModule)
+    }
+];
---
+++ src/app/modules/global/global.routing.ts
@@ -0,0 +1,10 @@
+import { Routes } from "@angular/router";
+import { Index } from "./components/index";
+
+export const globalRoutes: Routes = [
+    {
+        path: '',
+        component: Index,
+        loadChildren: ()=> import('./global-module').then(m=> m.GlobalModule)
+    }
+];
