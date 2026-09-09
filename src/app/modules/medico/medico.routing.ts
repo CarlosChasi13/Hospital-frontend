15a89a811ec89f6c96dfa2f16c0090d1e1a1f046
CarlosChasi13 <cachasi3@espe.edu.ec>
2026-09-08 21:49:50 -0500

    route medico

---
+++ src/app/modules/medico/medico.routing.ts
@@ -0,0 +1,10 @@
+import { Routes } from "@angular/router";
+import { Index } from "./components/index";
+
+export const medicoRoutes: Routes = [
+    {
+        path: 'medico/index',
+        component: Index,
+        loadChildren: ()=> import('./medico-module').then(m=> m.MedicoModule)
+    }
+];
--- src/app/rutas.module.ts
+++ src/app/rutas.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 import { globalRoutes } from "./modules/global/global.routing";
+import { medicoRoutes } from "./modules/medico/medico.routing";
 
 @NgModule({
     imports: [
         RouterModule.forChild([
-            ...globalRoutes
+            ...globalRoutes,
+            ...medicoRoutes
         ])],
         exports: [RouterModule]
 })
